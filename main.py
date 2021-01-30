1517429cc7b585a42b219f7088cc50a32c28ba04
Sarvasv Arora <sarvasvarora@gmail.com>
2021-01-30 18:40:42 +0530

    test commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import json
+import requests
+
+
+def parse_content(req):
+
+    pass
+
+
+def create_summary():
+    pass
+
+###### OPTIONAL (TIME PERMITTING) #####
+
+# Name entity recognition tags
+
+
+def ner_tags():
+    pass
+
+#
